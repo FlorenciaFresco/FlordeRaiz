2edc846a859deceb4a80628d2848b1a19f773094
Florencia Fresco <110852312+FlorenciaFresco@users.noreply.github.com>
2023-04-20 21:33:40 -0300

    Add files via upload

---
+++ src/component/Item/Item.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "./Item.css";
+
+const Item = ({ id, name, img, price, stock }) => {
+    return (
+        <div className="col1">
+            <article className="CardItem">
+                <header className="Header">
+                    <h2 className="ItemHeader">{name}</h2>
+                </header>
+                <picture>
+                    <img src={img} alt="name" className="ItemImg" />
+                </picture>
+                <section>
+                    <p className="Info">Precio: ${price}</p>
+                    <p className="Info">Stock Disponible: {stock}</p>
+                </section>
+                <footer className="ItemFooter">
+                    <Link to={`/item/${id}`} className="Option">
+                        Ver mas
+                    </Link>
+                </footer>
+            </article>
+        </div>
+    );
+};
+
+export default Item;
---
+++ src/component/itemCount/itemCount.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./itemCount.css";
+import { useState } from "react";
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+    const [quantity, setQuantity] = useState(initial);
+
+    const increment = () => {
+        if (quantity < stock) {
+            setQuantity(quantity + 1);
+        }
+    };
+
+    const decrement = () => {
+        if (quantity > 1) {
+            setQuantity(quantity - 1);
+        }
+    };
+
+    return (
+        <div class="Counter">
+            <div class="Controls">
+                <button class="Button" onClick={decrement}>
+                    -
+                </button>
+                <h4 class="Number">{quantity}</h4>
+                <button class="Button" onClick={increment}>
+                    +
+                </button>
+            </div>
+            <div>
+                <button
+                    class="Button"
+                    onClick={() => onAdd(quantity)}
+                    disabled={!stock}>
+                    Agregar al carrito
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ItemCount;
